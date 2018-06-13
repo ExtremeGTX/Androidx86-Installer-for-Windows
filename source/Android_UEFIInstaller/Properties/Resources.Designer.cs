4d4c0165406c415aee86c67a935b128cf5b64400
ExtremeGTX <ExtremeGTX@hotmail.com>
2018-06-13 21:36:03 +0200

    New UI Design

--- source/Android_UEFIInstaller/Properties/Resources.Designer.cs
+++ source/Android_UEFIInstaller/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Android_UEFIInstaller.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap android_text {
+            get {
+                object obj = ResourceManager.GetObject("android_text", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
